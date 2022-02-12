6098ee3391f3483c4af12770b64ea89c7328d3be
DaikiOkada <d.okada0820@gmail.com>
2022-02-12 12:54:24 +0900

    done

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# ServiceWorker
+### サービスワーカーの紹介
+
+https://developers.google.com/web/fundamentals/primers/service-workers?hl=ja
+
+### ウェブアプリへのプッシュ通知の追加
+
+https://developers.google.com/web/fundamentals/codelabs/push-notifications?hl=ja
+
+### バックグラウンド アプリにテスト メッセージを送信する
+
+https://firebase.google.com/docs/cloud-messaging/js/first-message?hl=ja
---
+++ firebase-messaging-sw.js
@@ -0,0 +1,21 @@
+self.addEventListener("push", function (event) {
+  console.log("[Service Worker] Push Received.");
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = "メッセージが来たで";
+  const options = {
+    body: "ここに本文が入るで",
+    icon: "images/icon.png",
+    badge: "images/badge.png",
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener("notificationclick", function (event) {
+  console.log("[Service Worker] Notification click Received.");
+
+  event.notification.close();
+
+  event.waitUntil(clients.openWindow("https://developers.google.com/web/"));
+});
